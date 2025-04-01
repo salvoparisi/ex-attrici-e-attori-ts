220af9dcd4342777222aa68f7a22d68cfa18f5f3
salvoparisi <psalvo065@gmail.com>
2025-04-02 01:46:37 +0200

    milestone 2

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,8 @@ type Person = {
   image: string
 }
 
+type Actress = {
+  most_famous_movies: [string, string, string],
+  awards: string,
+  nationality: "American" | "British" | "Australian" | "Israeli-American" | "South African" | "French" | "Indian" | "Israeli" | "Spanish" | "South Korean" | "Chinese"
+}
